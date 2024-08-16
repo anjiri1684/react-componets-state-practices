b0d98ed9dfb775bbacd2e060d642c2be89845a06
vincent anjiri <vincentanjiri12@gmail.com>
2024-08-16 12:13:22 +0300

    initial commit

--- src/components/UserList/UsersList.js
+++
@@ -1,5 +0,0 @@
-function UserList() {
-  return <>UserList</>;
-}
-
-export default UserList;
---
+++ src/components/Users/UserList/UsersList.js
@@ -0,0 +1,18 @@
+import Card from "../../UI/Card";
+import classes from "./UserList.module.css";
+
+function UserList(props) {
+  return (
+    <Card className={classes.users}>
+      <ul>
+        {props.users.map((user) => (
+          <li key={user.id}>
+            {user.name} ({user.age}years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+}
+
+export default UserList;
